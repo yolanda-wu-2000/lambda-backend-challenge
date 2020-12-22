209a596a9e3e2a6f350e2f5111668a9371b126f7
yolanda-wu-2000 <yolanda.wu.2000@gmail.com>
2020-12-22 00:35:27 -0800

    The first working version

---
+++ src/lambdas/breeds-get.ts
@@ -0,0 +1,45 @@
+import fetch from 'node-fetch'
+import { Response } from './types'
+
+interface BreedsResponse extends Response {
+  body: Breeds
+}
+
+interface Breeds {
+  message: string[]
+  status: string
+}
+
+function parseBreeds(message: object): string[] {
+  const breeds: string[] = []
+
+  Object.entries(message).forEach(element => {
+    const key = element[0]
+    const value = element[1]
+
+    if (value.length === 0) {
+      breeds.push(`${key}`)
+    } else {
+      value.forEach((sub: string) => {
+        breeds.push(`${sub} ${key}`)
+      })
+    }
+  })
+
+  return breeds
+}
+
+export async function handler(): Promise<BreedsResponse> {
+  const res = await fetch('https://dog.ceo/api/breeds/list/all')
+  if (!res.ok) {
+    throw new Error(`HTTP Error Response: ${res.status} ${res.statusText}`)
+  }
+
+  const json = await res.json()
+  const breedsMessage: string[] = await parseBreeds(json.message)
+  const payload: Breeds = { message: breedsMessage, status: json.status as string }
+  return {
+    statusCode: 200,
+    body: payload,
+  }
+}
